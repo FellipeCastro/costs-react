f038e4c1445cd929c534da0b718ea410327a67ac
FellipeCastro <fellipecastro27d@gmail.com>
2023-11-13 18:34:00 -0300

    estrutura base finalizada

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
+
+import Container from './components/layout/Container';
+ 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/Company">Empresa</Link>
+        <Link to="/NewProject">Home</Link>
+      </div>
+      <Container customClass="min_height">
+        <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/company" element={<Company />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/newproject" element={<NewProject />} />
+      </Routes>
+      </Container>
+
+      <p>Footer</p>
+    </Router>
   );
 }
 
