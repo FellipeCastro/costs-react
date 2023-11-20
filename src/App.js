dc249eb13b41eb51d4e2190d588673f3d1a6bc27
FellipeCastro <fellipecastro27d@gmail.com>
2023-11-20 15:31:57 -0300

    Primeira parte feita

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Contact from './components/pages/Contact';
 import Company from './components/pages/Company';
 import NewProject from './components/pages/NewProject';
 import Projects from './components/pages/Projects';
+import Project from './components/pages/Project';
 
 /*Layout*/
 import Container from './components/layout/Container';
@@ -22,6 +23,7 @@ function App() {
         <Route path="/company" element={<Company />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/newproject" element={<NewProject />} />
+        <Route path="/project/:id" element={<Project />} />
       </Routes>
       </Container>
       <Footer />
